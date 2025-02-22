940cb3b8d7b8d5c52fb90f03acb7533a84b297d2
Mohamed Badr <115354829+absallam1999@users.noreply.github.com>
2025-02-22 16:13:31 +0200

    V1

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -6,7 +6,7 @@ const request = supertest(app)
 describe('Test EndPoint API Response', () => {
   it('Should Get API StatusCode 200', async () => {
     const response = await request.get(
-      '/api/images/output/?filename=fjord&width=500&height=500'
+      '/api/images/?filename=fjord&width=500&height=500'
     )
     expect(response.statusCode).toBe(200)
   })
