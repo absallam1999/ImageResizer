a9e6e0a9b2e50c757cb634dd10d73717d29af425
Mohamed Badr <115354829+absallam1999@users.noreply.github.com>
2025-02-22 16:15:08 +0200

    V1

--- dist/router/logger.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-const logger = (req, res, next) => {
-  const url = req.url
-  console.log(`${url} \n was visted!`)
-  next()
-}
-exports.default = logger
